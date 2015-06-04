ed622f1fd8a78484893195ed28df5fe261a9032b
Melinda Seckington <mseckington@gmail.com>
2015-06-04 17:43:35 +0100

    Display stats of answered questions

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -5,8 +5,28 @@ class QuestionsController < ApplicationController
   end
 
   def show
-    @question = Question.find_by_id(params[:id])
-    @answer = Answer.new
+    @answer = Answer.find_or_create_by(user: current_user, question: question)
   end
 
+  def answer_response
+    if @answer.response
+      "You've answered 'Yes'"
+    else
+      "You've answered 'No'"
+    end
+  end
+
+  def question
+    @question ||= Question.find_by_id(params[:id])
+  end
+
+  def yes_responses_count
+    question.answers.where(response: true).count
+  end
+
+  def no_responses_count
+    question.answers.where(response: false).count
+  end
+
+  helper_method :answer_response, :question, :yes_responses_count, :no_responses_count
 end
