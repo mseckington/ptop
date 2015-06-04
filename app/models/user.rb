9f28140e250ea9f8b32ff2368a9a6737bfa69e64
Melinda Seckington <mseckington@gmail.com>
2015-06-04 17:02:31 +0100

    Introduce Answers model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ActiveRecord::Base
   validates :name, presence: true
   validates :email, presence: true, uniqueness: true
+
+  has_many :answers
+  has_many :questions, through: :answers
 end
