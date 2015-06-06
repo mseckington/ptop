8b3a68cb77617416b9789f9b929896e4c515d5b6
Melinda Seckington <mseckington@gmail.com>
2015-06-06 13:50:30 +0100

    WIP

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,4 +1,25 @@
 class Question < ActiveRecord::Base
+  ANSWER_THRESHOLD = 1
+
   has_many :answers
   has_many :users, through: :answers
+
+  def threshold_reached?
+    answers.count > ANSWER_THRESHOLD
+  end
+
+  def majority_agreed?
+    total = answers.count
+    yes_total = answers.where(response: true).count
+    no_total = answers.where(response: false).count
+
+    yes_percentage = yes_total.to_f / total * 100
+    no_percentage = no_total.to_f / total * 100
+
+    (yes_percentage > 90) || (no_percentage > 90)
+  end
+
+  def resolved?
+    threshold_reached? && majority_agreed?
+  end
 end
